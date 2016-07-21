6a47ea83c6ea00500c93671f28e90c2450b29f89
grantsu622 <su.tsunghsien@gmail.com>
2016-07-21 11:47:49 +0800

    1.修改存檔路徑為來源檔名 2.檔案格式修改 3. 測試中文

--- catchword-txt.py
+++ catchword-txt.py
@@ -1,23 +1,34 @@
+#-*- coding: cp950 -*-　
+#-*- coding: utf-8 -*-
+
 import urllib2
+
 import urllib
 import re
 import codecs
 import sys
+import os
 
-'''
+
+"""
 argv[1]:source file
 argv[2]:destination file
-'''
+"""
+#建立來源資料夾
+foldname = sys.argv[2][0:len(sys.argv[2])-4]
+if not os.path.exists(foldname):
+    os.makedirs(foldname)
+
 #f = open('b.txt', 'r')
 f = open(sys.argv[1], 'r')
 #fw = open('c.txt', 'w')
 #fw = codecs.open('c.txt', 'w', 'utf-8')
-fw = codecs.open(sys.argv[2], 'w', 'utf-8')
+fw = codecs.open(foldname + "/" + sys.argv[2], 'w', 'utf-8')
+flose = codecs.open(foldname + "/" + 'lose.txt', 'w', 'utf-8')
 
-'''
-Parse word
-'''
 
+
+#Parse word 分解單字
 from sgmllib import SGMLParser  
 class GetIdList(SGMLParser):  
     def reset(self):  
@@ -50,10 +61,12 @@ class GetIdList(SGMLParser):
             self.IDlist.append(text)  
     def printID(self):  
         for i in self.IDlist:  
-            print i.decode('utf-8') 
+            #print i.decode('utf-8') 
             #fw.write(i.deconde('utf-8'))
             i1 = unicode(i, 'utf-8')
-            fw.write(i1)
+            fw.write(i1[3:])
+            print i1[3:] #列印解釋
+
             fw.write('\n')
             #return i.decode('utf-8')
             
@@ -61,10 +74,9 @@ class GetIdList(SGMLParser):
 while True :
     i = f.readline()
     if i=='': break
-    print i
-    print len(i)
+    print i[0:len(i)-1]
+    #print len(i)
     
-    fw.write(i)
     
     url = 'https://tw.dictionary.yahoo.com/dictionary?p=' + i
     content = urllib2.urlopen(url).read()
@@ -74,17 +86,21 @@ while True :
     '''
     urls = re.findall('http[s]?://s.yimg.com/tn/dict/dreye/live/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+.mp3', content)
     try:
-        print urls[0]
+        #print urls[0]
+        len(urls[0])
     except IndexError:
+        flose.write(i)
         continue
-    urllib.urlretrieve(urls[0], i[0:len(i)-1] + ".mp3")
+    urllib.urlretrieve(urls[0], foldname + "/" + i[0:len(i)-1] + ".mp3")
     
     explanation = GetIdList()
     explanation.feed(content)
     #fistexplanation = explanation.printID()
-    explanation.printID()
-    fw.write('\n')
+    fw.write(i[0:len(i)-1] + " ")  #寫入英文單字
+    explanation.printID() #寫入註解
+    #fw.write('\n')
     #print fistexplanation
     
 f.close()
 fw.close()
+flose.close()
